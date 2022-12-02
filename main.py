9bb5003a28da9946a1bd70a8ce7629c84edabc93
David Kalliecharan <david@definedate.net>
2022-12-02 11:44:03 -0400

    Basic plotting functionality implemented.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-# This Python file uses the following encoding: utf-8
+#! /usr/bin/env python3
+# coding: utf-8
 import sys
 import os
 
-
 from PySide2.QtWidgets import (
-    QApplication, 
+    QApplication,
     QMainWindow,
     QVBoxLayout,
-    QWidget
+    QWidget,
+    QFileDialog
 )
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
@@ -20,6 +21,18 @@ from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
 )
 from matplotlib.figure import Figure
+import numpy as np
+from pandas import DataFrame
+from pathlib import Path
+from scipy.fft import fft, fftfreq
+
+from bin2data import read_bin
+
+
+ADC_COUNT_MAX = 4096 # Count
+VOLTAGE_MAX = 3.3 # V
+COUNTS_TO_VOLTS = VOLTAGE_MAX / ADC_COUNT_MAX # V / Count
+SAMPLE_RATE_KHZ_DFLT = 180 # 180 kHz
 
 
 class MPLCanvas(FigureCanvasQTAgg):
@@ -30,6 +43,8 @@ class MPLCanvas(FigureCanvasQTAgg):
 
 
 class PWJPlot(QMainWindow):
+    df = None
+    df_fft = None
     def __init__(self):
         super(PWJPlot, self).__init__()
         self.load_ui()
@@ -40,18 +55,18 @@ class PWJPlot(QMainWindow):
         path = os.path.join(os.path.dirname(__file__), "pwjplot.ui")
         ui_file = QFile(path)
         ui_file.open(QFile.ReadOnly)
-        ui = loader.load(ui_file, self)
+        self.ui = loader.load(ui_file, self)
         ui_file.close()
 
         self.fig_raw = MPLCanvas(self, width=5, height=4, dpi=100)
         self.fig_raw.axes.set_xlabel("Time (s)")
         self.toolbar_raw = NavigationToolbar(self.fig_raw, self)
-        
+
         layout_raw = QVBoxLayout()
         layout_raw.addWidget(self.fig_raw)
         layout_raw.addWidget(self.toolbar_raw)
-        ui.wgt_raw.setLayout(layout_raw)
-        
+        self.ui.wgt_raw.setLayout(layout_raw)
+
         self.fig_fft = MPLCanvas(self, width=5, height=4, dpi=100)
         self.fig_fft.axes.set_xlabel("Frequency (kHz)")
         self.toolbar_fft = NavigationToolbar(self.fig_fft, self)
@@ -59,9 +74,105 @@ class PWJPlot(QMainWindow):
         layout_fft = QVBoxLayout()
         layout_fft.addWidget(self.fig_fft)
         layout_fft.addWidget(self.toolbar_fft)
-        ui.wgt_fft.setLayout(layout_fft)
-        
-        self.setCentralWidget(ui.wgt_central)
+        self.ui.wgt_fft.setLayout(layout_fft)
+
+        self.ui.combo_fft_scale.addItems(['Linear', 'Log'])
+
+        self.ui.btn_file.clicked.connect(self.set_file)
+        self.ui.btn_load.clicked.connect(self.load_dataframe)
+        self.ui.btn_fft.clicked.connect(self.calculate_fft)
+        self.ui.combo_fft_scale.currentIndexChanged.connect(self.set_scale)
+
+        #self.setWindowIcon(QIcon('icon.png')
+        self.setWindowTitle("PWJPlot")
+        self.setCentralWidget(self.ui.wgt_central)
+
+    def set_scale(self):
+        scale = self.ui.combo_fft_scale.currentText().lower()
+        print(f"Setting FFT y scale to {scale}")
+        self.fig_fft.axes.set_yscale(f"{scale}")
+        self.fig_fft.draw()
+
+    def set_file(self):
+        filename = QFileDialog.getOpenFileName(
+            self,
+            caption=str("Open Binary File"),
+            filter=str("Binary files (*.bin, *.BIN)")
+        )
+        print(f"Setting filename: {filename[0]}")
+
+        self.ui.filename.setText(filename[0])
+
+    def load_dataframe(self):
+        print("Loading data")
+        if not Path(self.ui.filename.text()).is_file():
+            print("Please specify a file!")
+            return
+        x = read_bin(self.ui.filename.text())
+
+        # Create a cumulative sum of time from sample rate
+        self.sample_rate_hz = self.ui.sample_rate_khz.value() * 1e3
+        dt = 1 / self.sample_rate_hz
+        t = np.ones(x.shape)
+        t = np.cumsum(t * dt)
+
+        data = {
+            "Time (s)"       : t,
+            "ADC value"      : x,
+            "Voltage (V)"    : x * COUNTS_TO_VOLTS,
+            "Pressure (MPa)" : x,
+        }
+
+        self.df = DataFrame.from_dict(data)
+        print(self.df.head())
+
+        self.ui.fft_start.setValue(self.df["Time (s)"].min())
+        self.ui.fft_end.setValue(self.df["Time (s)"].max())
+
+        self.fig_raw.axes.cla()
+        self.fig_fft.axes.cla()
+
+        self.df.plot(x="Time (s)", y="ADC value", ax=self.fig_raw.axes)
+        self.fig_raw.draw()
+
+    def calculate_fft(self):
+        print("Calculating FFT")
+        self.fig_fft.axes.cla()
+
+        x_min = self.ui.fft_start.value()
+        x_max = self.ui.fft_end.value()
+
+        # Generate mask within specified time range
+        mask = self.df["Time (s)"] > x_min
+        mask &= self.df["Time (s)"] < x_max
+
+        x = self.df['ADC value'][mask]
+        t = self.df['Time (s)'][mask]
+        dt = t.diff().iloc[-1]
+
+        X = fft(x)
+        X = np.abs(X)
+        f = fftfreq(X.size, dt)
+
+        data = {'FFT(ADC value)' : X, 'Frequency (kHz)' : f / 1e3}
+        self.df_fft = DataFrame.from_dict(data)
+
+        self.fig_fft.axes.vlines(
+            x=self.df_fft['Frequency (kHz)'],
+            ymin=0,
+            ymax=self.df_fft['FFT(ADC value)'],
+            color='C0'
+        )
+        self.df_fft.plot(
+            x="Frequency (kHz)",
+            y="FFT(ADC value)",
+            color='C0',
+            marker='o',
+            linestyle='none',
+            markerfacecolor='white',
+            ax=self.fig_fft.axes
+        )
+        self.fig_fft.draw()
 
 
 if __name__ == "__main__":
