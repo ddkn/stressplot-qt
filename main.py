e3e56e8bbcef260a0145b01de31965e55100901e
David Kalliecharan <david@definedate.net>
2023-02-26 13:26:00 -0400

    Conversion to stressplot for quick viewing
    This currently omits any FFT analysis

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ from PySide2.QtWidgets import (
     QMainWindow,
     QVBoxLayout,
     QFileDialog,
+    QProgressBar,
 )
 from PySide2.QtCore import QFile
 from PySide2.QtGui import QIcon
@@ -52,20 +53,17 @@ from matplotlib.widgets import SpanSelector
 import numpy as np
 from pandas import DataFrame
 from pathlib import Path
-from scipy.fft import fft, fftfreq
-from bin2data import read_bin
 
-COLOR = "C1"
-ADC_COUNT_MAX = 4096  # Count
-VOLTAGE_MAX = 3.3  # V
-COUNTS_TO_VOLTS = VOLTAGE_MAX / ADC_COUNT_MAX  # V / Count
-SAMPLE_RATE_KHZ_DFLT = 180  # 180 kHz
-RG_OPTIONS = [50, 75, 100, 150, 300, 500, 750, 1000]
-RG_DEFAULT_INDEX = 1
+from waterjetstress import WaterJetStress
+
+COLOR = "C0"
+CROSS_COLOR = "C1"
+
+X_COLUMNS = ["Time (s)", "x (mm)"]
 
 # Matplotlib configuration
 mpl.use("Qt5Agg")
-style.use("pwjplot.mplstyle")
+style.use("stressplot.mplstyle")
 
 
 class MPLCanvas(FigureCanvasQTAgg):
@@ -75,74 +73,50 @@ class MPLCanvas(FigureCanvasQTAgg):
         super(MPLCanvas, self).__init__(fig)
 
 
-class PWJPlot(QMainWindow):
-    df: DataFrame = DataFrame()
-    df_fft: DataFrame = DataFrame()
-
+class StressPlot(QMainWindow):
     def __init__(self):
-        super(PWJPlot, self).__init__()
+        super(StressPlot, self).__init__()
         self.load_ui()
 
     def load_ui(self):
         loader = QUiLoader()
         path = Path(__file__).parent
-        ui_file = QFile(str(path / "pwjplot.ui"))
+        ui_file = QFile(str(path / "stressplot.ui"))
         ui_file.open(QFile.ReadOnly)
         self.ui = loader.load(ui_file, self)
         ui_file.close()
 
-        self.fig_raw = MPLCanvas(self, width=5, height=4, dpi=100)
-        self.fig_raw.axes.set_xlabel("Time (s)")
-        self.toolbar_raw = NavigationToolbar(self.fig_raw, self)
+        self.fig_data = MPLCanvas(self, width=5, height=4, dpi=90)
+        self.fig_data.axes.set_xlabel("Time (s)")
+        self.toolbar_data = NavigationToolbar(self.fig_data, self)
 
-        layout_raw = QVBoxLayout()
-        layout_raw.addWidget(self.fig_raw)
-        layout_raw.addWidget(self.toolbar_raw)
-        self.ui.wgt_raw.setLayout(layout_raw)
+        layout_data = QVBoxLayout()
+        layout_data.addWidget(self.fig_data)
+        layout_data.addWidget(self.toolbar_data)
+        self.ui.wgt_data.setLayout(layout_data)
 
-        self.fig_fft = MPLCanvas(self, width=5, height=4, dpi=100)
-        self.fig_fft.axes.set_xlabel("Frequency (kHz)")
-        self.toolbar_fft = NavigationToolbar(self.fig_fft, self)
-
-        layout_fft = QVBoxLayout()
-        layout_fft.addWidget(self.fig_fft)
-        layout_fft.addWidget(self.toolbar_fft)
-        self.ui.wgt_fft.setLayout(layout_fft)
-
-        self.ui.gain_resistor.addItems([str(r) for r in RG_OPTIONS])
-        self.ui.gain_resistor.setCurrentIndex(RG_DEFAULT_INDEX)
-        self.ui.combo_fft_scale.addItems(["Linear", "Log"])
         self.ui.output_path.setText(str(Path.home()))
 
-        self.ui.btn_fft.setDisabled(True)
         self.ui.btn_save.setDisabled(True)
 
         self.ui.btn_file.clicked.connect(self.set_file)
         self.ui.btn_data_out.clicked.connect(self.select_output_path)
         self.ui.btn_save.clicked.connect(self.save_data)
-        self.ui.btn_load.clicked.connect(self.load_dataframe)
-        self.ui.btn_fft.clicked.connect(self.calculate_fft)
-        self.ui.combo_data.currentIndexChanged.connect(self.change_ydata)
-        self.ui.combo_fft_scale.currentIndexChanged.connect(self.set_fft_scale)
+        self.ui.btn_load.clicked.connect(self.analyze)
+        self.ui.combo_xaxis.currentIndexChanged.connect(self.update_plot)
+        self.ui.combo_yaxis.currentIndexChanged.connect(self.update_plot)
 
         self.setWindowIcon(QIcon(str(path / "assets/icon.svg")))
-        self.setWindowTitle("PWJPlot")
+        self.setWindowTitle("StressPlot")
         self.setCentralWidget(self.ui.wgt_central)
-
-    def set_fft_scale(self):
-        scale = self.ui.combo_fft_scale.currentText().lower()
-        print(f"Setting FFT y scale to {scale}")
-        self.fig_fft.axes.set_yscale(f"{scale}")
-        self.fig_fft.draw()
 
     def set_file(self):
         filename = QFileDialog.getOpenFileName(
             self,
-            caption=str("Open Binary File"),
-            filter=str("Binary files (*.bin, *.BIN)"),
+            caption=str("Open CSV File"),
+            filter=str("CSV files (*.csv)"),
         )
         print(f"Setting filename: {filename[0]}")
-
         self.ui.filename.setText(filename[0])
 
     def select_output_path(self):
@@ -156,57 +130,45 @@ class PWJPlot(QMainWindow):
             return
 
         print(f"Setting output path: {output_path}")
-
         self.ui.output_path.setText(output_path)
 
-    def load_dataframe(self):
+    def analyze(self):
         print("Loading data")
         input_file = Path(self.ui.filename.text())
         if not input_file.is_file():
             print("Please specify a file!")
             return
-        x = read_bin(input_file)
 
-        # Create a cumulative sum of time from sample rate
-        self.sample_rate_hz = self.ui.sample_rate_khz.value() * 1e3
-        dt = 1 / self.sample_rate_hz
-        t = np.ones(x.shape)
-        t[0] = 0.0
-        t = np.cumsum(t * dt)
-
-        data = {
-            "Time (s)":       t,
-            "ADC value":      x,
-            "Voltage (V)":    x * COUNTS_TO_VOLTS,
-            "Pressure (MPa)": x,
-        }
-
-        self.df = DataFrame.from_dict(data)
-        print(self.df.head())
-
-        self.ui.combo_data.clear()
-        self.ui.combo_data.addItems(
-            self.df.drop("Time (s)", axis=1).columns.to_list()
+        vtr = self.ui.vtr.value()
+        dia_app = self.ui.dia_app.value()
+        self.stress = WaterJetStress(
+            input_file, 
+            vtr, 
+            diameter_kws=dict(dia_app=dia_app, scaled=True)
         )
 
-        self.fig_fft.axes.cla()
-        self.fig_fft.draw()
-        self.df_fft = DataFrame()
+        # Create a cumulative sum of time from sample rate
+        print(self.stress.df.head())
 
-        self.ui.fft_start.setValue(self.df["Time (s)"].min())
-        self.ui.fft_end.setValue(self.df["Time (s)"].max())
+        self.ui.combo_xaxis.clear()
+        self.ui.combo_yaxis.clear()
+        
+        self.ui.combo_xaxis.addItems(X_COLUMNS)
+        self.ui.combo_yaxis.addItems(
+            self.stress.df.columns.drop(X_COLUMNS).to_list()
+        )
 
-        self.ui.btn_fft.setDisabled(False)
         self.ui.btn_save.setDisabled(False)
+        self.update_plot()
 
     def save_data(self):
         input_file = Path(self.ui.filename.text())
-        filename = input_file.name.lower().replace(".bin", "")
+        filename = input_file.name.lower().replace(".csv", "")
 
-        rod_id = self.ui.rod_id.value()
-        gain_resistor = self.ui.gain_resistor.currentText().lower()
-        sample_rate = self.ui.sample_rate_khz.value()
-        name = f"{filename}_r{rod_id}_rg{gain_resistor}_{sample_rate}khz"
+        vtr = self.ui.vtr.value()
+        sd = self.ui.standoff_dist.value()
+        dia = self.ui.dia_app.value()
+        name = f"{filename}_vtr{vtr}_sd{sd}_dia{dia}"
 
         timestamp = pydt.now().strftime("%Y%b%d_%H:%M:%S:%f")
         output_name = f"{name}_{timestamp}.feather"
@@ -214,44 +176,67 @@ class PWJPlot(QMainWindow):
         output_path = Path(self.ui.output_path.text())
         output_file = output_path / output_name
         print(f"Saving {output_file}")
-        self.df.to_feather(output_file)
+        self.stress.df.to_feather(output_file)
 
-        if self.df_fft.shape[0] > 0:
-            output_fft_name = f"{name}_{timestamp}_fft.feather"
-            output_file = output_path / output_fft_name
-            print(f"Saving {output_file}")
-            self.df_fft.to_feather(output_file)
-
-    def change_ydata(self):
-        col = self.ui.combo_data.currentText()
-        if col not in self.df.columns.to_list():
+    def update_plot(self):
+        xlabel = self.ui.combo_xaxis.currentText()
+        if xlabel not in self.stress.df.columns:
+            return
+        ylabel = self.ui.combo_yaxis.currentText()
+        if ylabel not in self.stress.df.columns:
             return
 
-        print(f"Setting ydata to {col}")
+        print(f"Setting data to {xlabel} vs {ylabel}")
         self.span = None
-        self.fig_raw.axes.cla()
+        self.fig_data.axes.cla()
 
-        self.df.plot(
-            x="Time (s)",
-            y=col,
+        self.stress.df.plot(
+            x=xlabel,
+            y=ylabel,
             color=COLOR,
-            ax=self.fig_raw.axes,
+            ax=self.fig_data.axes,
             legend=False,
         )
-        self.fig_raw.axes.set_ylabel(col)
-        self.fig_raw.draw()
+        
+        idx = self.stress.get_feature_indices()
+        xdata = self.stress.df[xlabel][idx["mid"]]
+        ydata = self.stress.df[ylabel][idx["mid"]]
+        
+        xmin, xmax = self.fig_data.axes.get_xlim()
+        ymin, ymax = self.fig_data.axes.get_ylim()
+        self.fig_data.axes.hlines(0, xmin, xmax, color=CROSS_COLOR, linestyle="--")
+        self.fig_data.axes.hlines(ydata, xmin, xmax, color=CROSS_COLOR, linestyle=":")
+        self.fig_data.axes.vlines(xdata, ymin, ymax, color=CROSS_COLOR, linestyle=":")
+        self.fig_data.axes.set_ylabel(ylabel)
+        self.fig_data.axes.grid()
+        xorigin = 0
+        if xlabel == "x (mm)":
+            xorigin = -28.0 
+        self.fig_data.axes.annotate(
+            f"{ydata:0.2f}", 
+            (xorigin, ydata * 1.2), 
+            color=CROSS_COLOR,
+            fontsize="large",
+        )
+        
+        pressure = self.ui.input_pressure.value()
+        vtr = self.ui.vtr.value()
+        sd = self.ui.standoff_dist.value()
+        dia_app = self.ui.dia_app.value()
+        title = f"{pressure:0.1f} kPSI - VTR: {vtr:0.1f} mm/s - SD: {sd} mm - $\\phi_{{\\rm app}}$ {dia_app:0.1f} mm"
+        self.fig_data.axes.set_title(title)
+        
+        self.fig_data.draw()
 
         def span_select(xmin, xmax):
-            x = self.df["Time (s)"]
+            x = self.stress.df["Time (s)"]
             idxmin, idxmax = np.searchsorted(x, (xmin, xmax))
             idxmax = min(x.shape[0] - 1, idxmax)
             x_min = x.iloc[idxmin]
             x_max = x.iloc[idxmax]
-            self.ui.fft_start.setValue(x_min)
-            self.ui.fft_end.setValue(x_max)
 
         self.span = SpanSelector(
-            self.fig_raw.axes,
+            self.fig_data.axes,
             span_select,
             "horizontal",
             useblit=True,
@@ -259,51 +244,9 @@ class PWJPlot(QMainWindow):
             props=dict(alpha=0.5, facecolor="C2", edgecolor="C2"),
         )
 
-    def calculate_fft(self):
-        print("Calculating FFT")
-        self.fig_fft.axes.cla()
-
-        x_min = self.ui.fft_start.value()
-        x_max = self.ui.fft_end.value()
-
-        # Generate mask within specified time range
-        mask = self.df["Time (s)"] > x_min
-        mask &= self.df["Time (s)"] < x_max
-
-        x = self.df["ADC value"][mask]
-        t = self.df["Time (s)"][mask]
-        dt = t.diff().iloc[-1]
-
-        X = fft(x)
-        X = np.abs(X)
-        f = fftfreq(X.size, dt)
-
-        data = {"Frequency (kHz)": f / 1e3, "FFT(ADC value)": X}
-        self.df_fft = DataFrame.from_dict(data)
-
-        self.fig_fft.axes.vlines(
-            x=self.df_fft["Frequency (kHz)"],
-            ymin=0,
-            ymax=self.df_fft["FFT(ADC value)"],
-            color=COLOR,
-        )
-        self.df_fft.plot(
-            x="Frequency (kHz)",
-            y="FFT(ADC value)",
-            color=COLOR,
-            marker="o",
-            linestyle="none",
-            markerfacecolor="white",
-            ax=self.fig_fft.axes,
-            legend=False,
-        )
-        self.set_fft_scale()
-        self.fig_fft.axes.set_ylabel("FFT(ADC value)")
-        self.fig_fft.draw()
-
 
 if __name__ == "__main__":
     app = QApplication([])
-    widget = PWJPlot()
+    widget = StressPlot()
     widget.show()
     sys.exit(app.exec_())
